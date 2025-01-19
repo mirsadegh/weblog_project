7dc192839977a54923080b43dadbd2fc27ba2f5d
Sadegh <mirsadeghporbehnam831@gmail.com>
2025-01-19 12:28:06 +0330

    add email to project

--- account/models.py
+++ account/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="ایمیل")
     is_author = models.BooleanField(
         default=False, verbose_name="وضعیت نویسندگی")
     special_user = models.DateTimeField(
